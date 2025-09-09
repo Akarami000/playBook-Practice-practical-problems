2f1aa033b59ecab63ab39ea810f423d972a0681c
Akarami000 <akaramiakshay@gmail.com>
2025-09-09 23:41:27 +0530

    completeting todo app logic

--- src/Projects/TodoApp/Todo.tsx
+++ src/Projects/TodoApp/Todo.tsx
@@ -1,24 +1,31 @@
-import { useContext, useState } from "react";
+import { useCallback, useContext, useState } from "react";
 import { contextProvider } from "../../ContextAPI/createContext";
 import { Input } from "@mui/material";
 import './Todo.css'
 import {v4 as uuid} from 'uuid'
 
-
+interface ListDataItem {
+  id:string,
+  text:string
+}
 const Todo = () => {
 const  {selectMode} = useContext(contextProvider)
  const [input,setInput] = useState("")
- const [listData,setListData]= useState<string[]>([])
-interface ListDataItem {
-    // Define the structure of your list items here if known
-    // For example: id: number; text: string;
-}
+ const [listData,setListData]= useState<ListDataItem[]>([])
+
 
 const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>): void => {
-    if (e.key === "Enter") {
-        setListData([...listData,input])
+    if (e.key === "Enter" && input.trim()!=="" ) {
+        setListData([...listData,{ id:uuid(),text:input}])
         setInput("")
     }
+}
+const handleOnEdit =useCallback((data:ListDataItem)=>{
+    setListData(listData.map((item)=> 
+        item.id===data.id ? {...item,text:data.text}:item))
+},[listData])
+const handleDelete = (data:any)=>{
+    setListData(listData.filter((item)=> item.id !== data.id))
 }
  return (
         <div className={`Todo-${selectMode}`}>
@@ -42,7 +49,7 @@ const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>): void => {
             ></Input>
 
             <div className={`Todo-list${selectMode}`}>
-                <TaskBar sendListData={listData} selectMode={selectMode}/>
+                <TaskBar onEdit={handleOnEdit} sendListData={listData} onDelete={handleDelete} selectMode={selectMode}/>
             </div>
         </div>
     )
@@ -50,36 +57,80 @@ const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>): void => {
 
 interface TaskBarProps {
     selectMode: string;
-    sendListData:string[];
+    sendListData:ListDataItem[];
+    onDelete:(item:string)=>void;
+    onEdit:(item:ListDataItem)=>void;
    
 }
-interface ShareData{
-    shareData:boolean
-}
+
 
 const TaskBar = (prop: TaskBarProps) => {
-    const handleCheckBox=(e:React.ChangeEvent<HTMLInputElement>)=>{
-        console.log(e.target.checked)
+    const [checkbox,setCheckBox] = useState<string[]>([]);
+    const [editInput,setEditInput]= useState<ListDataItem>({
+        id:"",
+        text:""
+    })
+    const handleCheckBox=(data:any)=>{
+        setCheckBox((previousCheckedItem:any)=>{
+            if(previousCheckedItem.includes(data)){
+                return previousCheckedItem.filter((item:Boolean)=> item !== data)
+            }else{
+                return [...previousCheckedItem,data]
+            }
+        })
+    }
+
+     
+    
+    
+    const handleKeyDown=(e:React.KeyboardEvent<HTMLInputElement>)=>{
+        if(e.key=== "Enter" && editInput.text.trim() !== ""){
+                prop.onEdit(editInput)
+
+             setEditInput({id:"",text:""})
+        }
+       
     }
     return (
         <>
       { prop.sendListData.map((data)=>{
+        const isChecked =  checkbox.includes(data);
+        const edit = editInput.id === data.id;
         return(
-             <div key={uuid()}  className={`taskBar-${prop.selectMode}`}  >
+             <div key={data.id}  className={`taskBar-${prop.selectMode}`} >
            <div className="checkboxWithText">
             <Input
                 
-                onChange={handleCheckBox}
+                onChange={()=>handleCheckBox(data)}
                 type="checkbox"
                 style={{
                     padding:"10px"
                 }}
+                defaultChecked={isChecked}
             />
-            <h3 style={{textDecoration: "line-through"}}> {data}</h3>
+            {!edit ?<h3 style={isChecked?{textDecoration: "line-through"}:{textDecoration:"None"}}> {data.text}</h3>:
+                <Input
+                value={editInput.text}
+                style={prop.selectMode==="Dark" ?{
+                width:'250px',
+                height:'30px',
+                border:"1px solid white",
+                color:"white"
+            }:{
+                 width:'250px',
+                height:'30px',
+                border:"1px solid black",
+                color:"black"
+            }}
+                type="text"
+                onChange={(e)=>setEditInput({...editInput,text:e.target.value})}
+                 onKeyDown={handleKeyDown}
+                />
+            }
             </div>
             <div>
-            <button className="button" style={{margin:"5px"}} >Edit</button>
-             <button className="button" style={{margin:"5px"}} >Delete</button>
+            <button className="button" onClick={()=>{setEditInput({id:data.id,text:data.text}) }} style={{margin:"5px"}} >Edit</button>
+             <button className="button"  style={{margin:"5px"}} onClick={()=>prop.onDelete(data)} >Delete</button>
             </div>
         </div>
         )
